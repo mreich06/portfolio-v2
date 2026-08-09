cd4579d6f77727a851ccd4c66c3e8e9f1351b49e
Maya Reich <mayareich0606@gmail.com>
2026-08-09 18:29:37 +0200

    add linting for testing

---
+++ src/components/NavModal/NavModal.module.css.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+// WARNING: THIS FILE IS AUTO GENERATED, PLEASE DO NOT EDIT IT MANUALLY.
+// prettier-ignore
+declare const styles: {
+  readonly "closeButton": string;
+  readonly "contactButton": string;
+  readonly "modal": string;
+  readonly "modalItem": string;
+};
+export = styles;
