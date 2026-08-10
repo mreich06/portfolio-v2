2641848ae2317ef33547b3e4833d6345d2fb8df4
Maya Reich <mayareich0606@gmail.com>
2026-08-10 19:07:44 +0200

    hero section layout;

---
+++ src/sections/Hero/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Hero.module.css';
+import Button from '../../components/Button';
+import image from '../../assets/profile-photo.webp';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroText}>
+        <h5 className="font-sans-lg color-cyan">Hello, I'm</h5>
+        <h1 className="font-sans-2xl weight-bold color-white">Maya Reich</h1>
+        <h3 className="font-sans-xl weight-bold color-muted">Snowboarding enthusiast on a code journey</h3>
+        <h4 className="font-sans-base color-white-70">
+          I am a detail-oriented and highly adaptable Full Stack Web and Mobile Software Engineer with 4 years of experience developing enterprise
+          applications.
+        </h4>
+        <div className={styles.buttonContainer}>
+          <Button title={'View my work'} variant="solid-primary" bold upperCase />
+          <Button title={'Get in touch'} variant="outline-secondary" bold upperCase />
+        </div>
+      </div>
+      <div className={styles.heroPhoto}>
+        <img className={styles.photo} src={image} alt="profile photo" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
