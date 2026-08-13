c6f8d57ea0449cfafaa87da1445ba55053e1d20b
Maya Reich <mayareich0606@gmail.com>
2026-08-13 21:32:08 +0200

    remove package breaking build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import TypedCssModules from 'unplugin-typed-css-modules/vite';
 
 export default defineConfig({
-  plugins: [react(), TypedCssModules({})],
+  plugins: [react()],
 });
