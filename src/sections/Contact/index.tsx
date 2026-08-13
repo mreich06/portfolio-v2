2bee507d836749d663b1d74e85ca2761729a804d
Maya Reich <mayareich0606@gmail.com>
2026-08-13 21:14:06 +0200

    setup

---
+++ src/sections/Contact/index.tsx
@@ -0,0 +1,11 @@
+import SectionHeader from '../../components/SectionHeader';
+
+const Contact = () => {
+  return (
+    <div>
+      <SectionHeader sectionNumber={'05. Contact'} title={"Let's build something."} />
+    </div>
+  );
+};
+
+export default Contact;
